3dd239fd7b483ab2df7908b331edf4730007ba36
Ikkei Itoku <ikkeiitoku@gmail.com>
2019-12-15 15:20:46 -0500

    Added sample feature and fixed the sort bug

--- source/utils.py
+++ source/utils.py
@@ -4,13 +4,17 @@ import pandas as pd
 from sklearn.metrics import make_scorer, mean_squared_error
 
 
-def train_test_split_feature(feature):
+def train_test_split_feature(feature, sample_n_users=None):
     """Split feature to train and split to predict the last rating for each user"""
+
+    if sample_n_users:
+        sampled_users = pd.Series(feature.user_id.unique()).sample(sample_n_users)
+        feature = feature[feature.user_id.isin(sampled_users)].copy()
 
     y_col = "review_stars"
     feature.review_date = pd.to_datetime(feature.review_date)
 
-    last_review = feature.groupby("user_id").tail(1)
+    last_review = feature.sort_values("review_date").groupby("user_id").tail(1)
     other_review = feature.drop(last_review.index)
 
     drop_cols = [y_col, "user_id", "review_date"]
