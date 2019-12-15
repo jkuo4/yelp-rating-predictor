c3a61d6f71286e2ea4cbcc824938239fd3334ee6
Ikkei Itoku <ikkeiitoku@gmail.com>
2019-12-15 15:32:33 -0500

    Updated train_test_split_feature to return all features without X and y

--- source/utils.py
+++ source/utils.py
@@ -11,20 +11,11 @@ def train_test_split_feature(feature, sample_n_users=None):
         sampled_users = pd.Series(feature.user_id.unique()).sample(sample_n_users)
         feature = feature[feature.user_id.isin(sampled_users)].copy()
 
-    y_col = "review_stars"
     feature.review_date = pd.to_datetime(feature.review_date)
-
     last_review = feature.sort_values("review_date").groupby("user_id").tail(1)
     other_review = feature.drop(last_review.index)
 
-    drop_cols = [y_col, "user_id", "review_date"]
-    X_test = last_review.drop(columns=drop_cols)
-    y_test = last_review[y_col]
-
-    X_train = other_review.drop(columns=drop_cols)
-    y_train = other_review[y_col]
-
-    return X_train, X_test, y_train, y_test
+    return other_review, last_review
 
 
 def rmse(y_true, y_pred):
