17ef3357dbf5ab3da444a719157e1821537bb772
Ikkei Itoku <ikkeiitoku@gmail.com>
2019-12-16 22:35:29 -0500

    Aligned API and file names

---
+++ source/fm.py
@@ -0,0 +1,28 @@
+import xlearn as xl
+
+
+class FMRecommender(object):
+    """
+    Extension of xlearn's Factorization Machines
+    """
+
+    def __init__(self, X_train, X_test, y_train, y_test):
+        self.model_file = "./model.txt"
+        self.weights_file = "./model_dm.out"
+
+        self.model = xl.create_fm()
+        self.model.setTXTModel(self.model_file)
+        self.model.setTrain(xl.DMatrix(X_train, y_train))
+        self.model.setTest(xl.DMatrix(X_test, y_test))
+
+    def fit(self, param):
+        """
+        Fit the modeli.
+
+        param: dict, parameters to fit the model with
+        """
+        self.model.fit(param, self.weights_file)
+
+    def predict(self):
+        """Predict ratings."""
+        return self.model.predict(self.weights_file)
