eb38fb640339ab67434a1c6947e1a407fe612faa
jkuo4 <jason.kuo4@gmail.com>
2019-12-20 01:54:13 -0500

    finished fm

--- source/fm.py
+++ source/fm.py
@@ -3,26 +3,24 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.datasets import dump_svmlight_file
 import xlearn as xl
 
-
 class FMRecommender(object):
     """
     Extension of xlearn's Factorization Machines
     """
 
-    def __init__(self, X_train, X_test, y_train, y_test, categorical_cols):
+    def __init__(self, X_train, y_train, categorical_cols, categorical_universe):
         self.model_file = "data/fm/model.txt"
         self.weights_file = "data/fm/model_wt.out"
         self.x_train_file = "data/fm/x_train.txt"
         self.x_test_file = "data/fm/x_test.txt"
         self.categorical_cols = categorical_cols
-
+        self.categorical_universe = categorical_universe
+        self.enc = OneHotEncoder().fit(self.categorical_universe.reshape(-1, 1))
+        
         self.model = xl.create_fm()
         self.model.setTXTModel(self.model_file)
         
-        X_train = self.one_hot_categorical_col(X_train)
-        X_test = self.one_hot_categorical_col(X_test)
-        dump_svmlight_file(X_train, y_train, self.x_train_file, zero_based=True, multilabel=False)
-        dump_svmlight_file(X_test, y_test, self.x_test_file, zero_based=True, multilabel=False)
+        self.transform_input(X_train, y_train, 'train')
         
 
     def fit(self, param):
@@ -33,14 +31,31 @@ class FMRecommender(object):
         """
         self.model.setTrain(self.x_train_file)
         self.model.fit(param, self.weights_file)
+        
+        
+    def transform_input(self, X, y, input_type):
+        """
+        Transform the raw file into sparse file for model
+
+        param: numpy.ndarray for the raw predictor values
+        param: numpy.ndarray for the raw target values
+        param: string, train/test
+        """
+        X_ohe_cat = self.one_hot_categorical_col(X)
+        if input_type == 'train':
+            file_path = self.x_train_file
+        else:
+            file_path = self.x_test_file
+        dump_svmlight_file(X_ohe_cat, y, file_path, zero_based=True, multilabel=False)
 
         
-    def predict(self):
+    def predict(self, X_test, y_test):
         """
         Predict ratings.
         
         return: numpy.ndarray for the predicted values
         """
+        self.transform_input(X_test, y_test, 'test')
         self.model.setTest(self.x_test_file)
         return self.model.predict(self.weights_file)
     
@@ -57,7 +72,6 @@ class FMRecommender(object):
         
         output = X.drop(self.categorical_cols, axis=1).astype(float)
         for col in self.categorical_cols:
-            enc = OneHotEncoder() # sparse format
-            ohe_col = enc.fit_transform(X[col].values.reshape(-1, 1))
+            ohe_col = self.enc.transform(X[col].values.reshape(-1, 1))
             output = hstack([output, ohe_col])            
         return output
